a36b296c60d5cd600b8e3c4b933d5cbef109a537
Kostya Ischuk <slubrusher@yandex.ru>
2024-07-18 14:29:15 +0700

    added UserInfo

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Card } from './components/Card';
 import { CardsContainer } from './components/CardsContainer';
 import { CardData } from './components/CardsData';
 import { UserData } from './components/UserData';
+import { UserInfo } from './components/UserInfo';
 
 import { Api } from './components/base/api';
 import { EventEmitter } from './components/base/events';
@@ -19,6 +20,7 @@ const api = new AppApi(baseApi);
 
 const cardsData = new CardData(events);
 const userData = new UserData(events);
+const userView = new UserInfo(document.querySelector('.profile'), events);
 
 
 const cardTemplate: HTMLTemplateElement = document.querySelector('.card-template');
@@ -33,15 +35,28 @@ Promise.all([api.getUser(), api.getCards()])
 .then(([userInfo, initialCards]) => {
   userData.setUserInfo(userInfo);
   cardsData.cards = initialCards;
+  events.emit('initialData:loaded')
   })
   .catch((err) => {
     console.error(err);
   });
 
-  const card = new Card(cloneTemplate(cardTemplate), events);
-  const card1 = new Card(cloneTemplate(cardTemplate), events);
-  const cardArray = [];
-  cardArray.push(card.render(testCards[0], testUser._id));
-  cardArray.push(card1.render(testCards[1], testUser._id));
+  // const card = new Card(cloneTemplate(cardTemplate), events);
+  // const card1 = new Card(cloneTemplate(cardTemplate), events);
+  // const cardArray = [];
+  // cardArray.push(card.render(testCards[0], testUser._id));
+  // cardArray.push(card1.render(testCards[1], testUser._id));
 
-  cardsContainer.render({catalog: cardArray});
+  // cardsContainer.render({catalog: cardArray});
+
+  // userView.render(testUser)
+
+  events.on('initialData:loaded', () => {
+    const cardsArray = cardsData.cards.map((card) => {
+      const cardInstant = new Card(cloneTemplate(cardTemplate), events);
+      return cardInstant.render(card, userData.id);
+    });
+
+    cardsContainer.render( { catalog: cardsArray } );
+    userView.render(userData.getUserInfo());
+  })
